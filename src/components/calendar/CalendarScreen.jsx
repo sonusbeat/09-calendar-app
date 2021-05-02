475c63d051724ac9166f8285ccca5f77d15c8085
qbixmex <qbixmex@gmail.com>
2021-05-02 18:09:13 -0500

    Instalación del Calendario

--- src/components/calendar/CalendarScreen.jsx
+++ src/components/calendar/CalendarScreen.jsx
@@ -1,9 +1,29 @@
-import Navbar from '../ui/Navbar';
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+import Navbar from "../ui/Navbar";
+
+const localizer = momentLocalizer(moment)
+
+const events = [{
+  title: "Meeting with UI",
+  start: moment().toDate(), // new Date()
+  end: moment().add( 2, "hours" ).toDate(),
+}];
 
 const CalendarScreen = () => {
   return (
-    <div>
+    <div className="calendar-screen">
       <Navbar />
+
+      <Calendar
+        localizer={ localizer }
+        events={ events }
+        startAccessor="start"
+        endAccessor="end"
+      />
+
     </div>
   );
 }
