dc670ef7b0585f048278795ae3c0a00f8e78f073
qbixmex <qbixmex@gmail.com>
2021-05-12 20:05:19 -0500

    Protección de rutas

---
+++ src/components/ui/Spinner.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+
+const Spinner = ({ msg }) => {
+  const [ message, setMessage ] = useState("Loading");
+
+  msg && setMessage(msg);
+
+  return (
+    <div
+      className="bg-dark d-flex justify-content-center align-items-center"
+      style={{ height: "100vh" }}
+    >
+      <div className="spinner-border text-success" role="status">
+        <span className="sr-only">{ message } ...</span>
+      </div>
+    </div>
+  );
+}
+
+export default Spinner;
--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,14 +1,18 @@
 import { useEffect } from "react";
-import { Redirect, Route, Switch } from "react-router";
-import { useDispatch } from 'react-redux';
+import { Redirect, Switch } from "react-router";
+import { useDispatch, useSelector } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import LoginScreen from '../components/auth/LoginScreen';
 import CalendarScreen from '../components/calendar/CalendarScreen';
 import { startChecking } from '../actions/auth';
+import Spinner from '../components/ui/Spinner';
+import PublicRoute from './PublicRoute';
+import PrivateRoute from './PrivateRoute';
 
 const AppRouter = () => {
   const dispatch = useDispatch();
+  const { checking, uid } = useSelector(state => state.auth);
 
   // Usamos el efecto secundario para disparar
   // la acción de startChecking que sirve si
@@ -17,12 +21,27 @@ const AppRouter = () => {
     dispatch( startChecking() );
   }, [dispatch]);
 
+  // Llama el Spinner si checking es verdadero
+  if (checking) return ( <Spinner /> );
+
   return (
     <Router>
       <div>
         <Switch>
-          <Route exact path="/login" component={ LoginScreen } />
-          <Route exact path="/" component={ CalendarScreen } />
+
+          <PublicRoute
+            exact
+            path="/login"
+            component={ LoginScreen }
+            isAuthenticated={ !!uid }
+          />
+
+          <PrivateRoute
+            exact
+            path="/"
+            component={ CalendarScreen }
+            isAuthenticated={ !!uid }
+          />
           <Redirect to="/" />
         </Switch>
       </div>
