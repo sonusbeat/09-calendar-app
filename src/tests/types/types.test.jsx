6b5553803c11bb55c43af710d7a8f6bcbed1e2ea
Daniel Gonzalez Briseño <qbixmex@gmail.com>
2021-06-02 13:45:53 -0500

    Test en types

---
+++ src/tests/types/types.test.jsx
@@ -0,0 +1,27 @@
+import types from "../../types/types";
+
+describe('Pruebas en Types', () => {
+
+    test('Deberia coincidir con los types', () => {
+        expect(types).toEqual({
+            uiOpenModal : "[ui] Modal opened",
+            uiCloseModal: "[ui] modal closed",
+
+            eventSetActive: "[event] Active event Setted",
+            eventLogout   : "[event] Logged Out",
+
+            eventStartAddNew     : "[event] Start add new",
+            eventAddNew          : "[event] Event Added",
+            eventClearActiveEvent: "[event] Event cleared",
+            eventUpdated         : "[event] Event updated",
+            eventDeleted         : "[event] Event deleted",
+            eventLoaded          : "[event] Events loaded",
+
+            authCheckingFinish: "[auth] Finish checking login state",
+            authLogin         : "[auth] Start login",
+            authStartRegister : "[auth] Start Register",
+            authLogout        : "[auth] Logout",
+        });
+    });
+    
+});
--- src/types/types.js
+++ src/types/types.js
@@ -1,22 +1,22 @@
 const types = {
 
-  uiOpenModal:  "[ui] Modal opened",
+  uiOpenModal : "[ui] Modal opened",
   uiCloseModal: "[ui] modal closed",
 
-  eventSetActive:        "[event] Active event Setted",
-  eventLogout:           "[event] Logged Out",
+  eventSetActive: "[event] Active event Setted",
+  eventLogout   : "[event] Logged Out",
 
-  eventStartAddNew:      "[event] Start add new",
-  eventAddNew:           "[event] Event Added",
+  eventStartAddNew     : "[event] Start add new",
+  eventAddNew          : "[event] Event Added",
   eventClearActiveEvent: "[event] Event cleared",
-  eventUpdated:          "[event] Event updated",
-  eventDeleted:          "[event] Event deleted",
-  eventLoaded:           "[event] Events loaded",
+  eventUpdated         : "[event] Event updated",
+  eventDeleted         : "[event] Event deleted",
+  eventLoaded          : "[event] Events loaded",
 
   authCheckingFinish: "[auth] Finish checking login state",
-  authLogin: "[auth] Start login",
-  authStartRegister: "[auth] Start Register",
-  authLogout: "[auth] Logout",
+  authLogin         : "[auth] Start login",
+  authStartRegister : "[auth] Start Register",
+  authLogout        : "[auth] Logout",
 
 };
 
